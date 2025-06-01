fac930a376f0e364e79afed7d1a7d30f246b9100
Katelynn Lee <krlee@andrew.cmu.edu>
2025-06-01 19:50:33 -0400

    Add counter functionality

--- src/RowCounter.js
+++ src/RowCounter.js
@@ -2,26 +2,29 @@ import Restart from './assets/restart.png';
 import Subtract from './assets/subtract.png';
 import Add from './assets/add.png';
 
+import {useState} from 'react';
+
 function RowCounter() {
+    const [rows, setRows] = useState(0);
     return (
         <div>
             <h1>
-                0
+                {rows}
             </h1>
             <h3>
                 Rows
             </h3>
             <div class="Counter-buttons-outer">
                 <div class="Counter-buttons">
-                    <button onClick={() => alert('Restart button clicked!')} class="Button" id="restart-button">
+                    <button onClick={() => setRows(0)} class="Button" id="restart-button">
                         <img src={Restart} alt="Restart" class="Counter-icon"/>
                     </button>
 
-                    <button onClick={() => alert('Subtract button clicked!')} class="Button" id="subtract-button">
+                    <button onClick={() => {if (rows > 0) setRows(rows - 1)}} class="Button" id="subtract-button">
                         <img src={Subtract} alt="Subtract" class="Counter-icon"/>
                     </button>
 
-                    <button onClick={() => alert('Add button clicked!')} class="Button" id="add-button">
+                    <button onClick={() => setRows(rows + 1)} class="Button" id="add-button">
                         <img src={Add} alt="Add" class="Counter-icon"/>
                     </button>
                 </div>
--- src/StitchCounter.js
+++ src/StitchCounter.js
@@ -2,26 +2,29 @@ import Restart from './assets/restart.png';
 import Subtract from './assets/subtract.png';
 import Add from './assets/add.png';
 
+import {useState} from 'react';
+
 function StitchCounter() {
+    const [stitches, setStitches] = useState(0);
     return (
         <div>
             <h1>
-                0
+                {stitches}
             </h1>
             <h3>
                 Stitches
             </h3>
             <div class="Counter-buttons-outer">
                 <div class="Counter-buttons">
-                    <button onClick={() => alert('Restart button clicked!')} class="Button" id="restart-button">
+                    <button onClick={() => setStitches(0)} class="Button" id="restart-button">
                         <img src={Restart} alt="Restart" class="Counter-icon"/>
                     </button>
 
-                    <button onClick={() => alert('Subtract button clicked!')} class="Button" id="subtract-button">
+                    <button onClick={() => {if (stitches > 0) setStitches(stitches - 1)}} class="Button" id="subtract-button">
                         <img src={Subtract} alt="Subtract" class="Counter-icon"/>
                     </button>
 
-                    <button onClick={() => alert('Add button clicked!')} class="Button" id="add-button">
+                    <button onClick={() => setStitches(stitches + 1)} class="Button" id="add-button">
                         <img src={Add} alt="Add" class="Counter-icon"/>
                     </button>
                 </div>
