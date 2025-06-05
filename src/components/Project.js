0c712db9f16f78af57894f57c0596e7f76c48e7b
Katelynn Lee <katelynnrae16@gmail.com>
2025-06-05 00:25:40 -0400

    Add notes title and icon

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,27 +4,36 @@ import StitchCounter from './StitchCounter';
 import { usePersistedState } from '../hooks/usePesistedState';
 import { useParams } from 'react-router-dom';
 
+import { MdDriveFileRenameOutline } from "react-icons/md";
+import { BiRename } from "react-icons/bi";
+
 function Project() {
     const {project} = useParams();
-    const [notes, setNotes] = usePersistedState(`notes/${project}`,"Notes")
+    const [notes, setNotes] = usePersistedState(`notes/${project}`,"Enter notes here...")
 
     return (
         <div class="Project">
             <h2>
-                {project}
+                <MdDriveFileRenameOutline /> &nbsp; {project}
             </h2>
             <div class="Counters">
                 <RowCounter />
                 <StitchCounter />
             </div>
-            <textarea 
-                id="notes" 
-                name="notes" 
-                rows="4" 
-                cols="50" 
-                value={notes}
-                onChange={e => setNotes(e.target.value)}>
-            </textarea>
+            <div class="Notes-container">
+                <div class="Notes">
+                    <BiRename class="Edit-icon"/>
+                    &nbsp;Notes
+                </div>
+                <textarea 
+                    id="notes" 
+                    name="notes" 
+                    rows="4" 
+                    cols="50" 
+                    value={notes}
+                    onChange={e => setNotes(e.target.value)}>
+                </textarea>
+            </div>
         </div>
     );
 }
