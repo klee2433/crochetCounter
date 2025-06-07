083e3a9a4eb3d8b0c99703d6b93f49333de4988b
Katelynn Lee <katelynnrae16@gmail.com>
2025-06-06 23:14:47 -0400

    Add clear notes feature

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,8 +6,11 @@ import { useParams } from 'react-router-dom';
 
 import { MdDriveFileRenameOutline } from "react-icons/md";
 import { BiRename } from "react-icons/bi";
+import { IoIosTrash } from "react-icons/io";
 
 import { FileUploader } from './FileUploader';
+import { useState } from 'react';
+import { Confirm } from 'react-admin';
 
 function Project() {
     const {project} = useParams();
@@ -16,6 +19,18 @@ function Project() {
 
     const handleFile = (file) => {
         setFileName(URL.createObjectURL(file))
+    };
+
+    const [open, setOpen] = useState(false);
+    const handleClick = () => {
+        setOpen(true);
+    };
+    const handleDialogClose = () => {
+        setOpen(false);
+    };
+    const handleConfirm = () => {
+        setNotes("");
+        setOpen(false);
     };
 
     return (
@@ -30,9 +45,58 @@ function Project() {
                 <StitchCounter />
             </div>
             <div className="Notes-container">
-                <div className="Notes">
-                    <BiRename className="Edit-icon"/>
-                    &nbsp; <div>Notes</div>
+                <div className="Notes-header">
+                    <div className="Notes">
+                        <BiRename className="Edit-icon"/>
+                        &nbsp; <div>Notes</div>
+                    </div>
+                    <button onClick={() => {handleClick()}} className="Button" id="clear-notes-button">
+                        <IoIosTrash className="Clear-icon"/>
+                    </button>
+                    <Confirm
+                        isOpen={open}
+                        title={"Clear Notes"}
+                        content="Are you sure you want to delete all notes?"
+                        onConfirm={handleConfirm}
+                        onClose={handleDialogClose}
+                        confirm="Yes, delete forever"
+                        cancel="No, do not delete"
+                        sx={{
+                            '& .MuiDialog-paper': {
+                                borderRadius:'3vw',
+                            },
+                            '& .MuiDialogTitle-root': {
+                                backgroundColor: 'rgb(186, 239, 189)',
+                                color: ' #4B4A67',
+                                padding:'2vw',
+                                fontFamily:'MonomaniacOne-Regular',
+                                fontSize:'3vw',
+                            },
+                            '& .MuiDialogContent-root': {
+                                color: ' #4B4A67',
+                                padding:'2vw',
+                                fontFamily:'MonomaniacOne-Regular',
+                                fontSize:'3vw'
+                            },
+                            '& .MuiDialogActions-root': {
+                                padding:'2vw',
+                            },
+                            '& .RaConfirm-confirmPrimary': { // Target the confirm button
+                                backgroundColor: ' #FD96A9',
+                                '&:hover': {
+                                backgroundColor: ' #f8bfca',
+                                },
+                            },
+                            '& .MuiButton-text': { // Target the cancel button
+                                color: ' #4B4A67',
+                                fontFamily:'MonomaniacOne-Regular',
+                                fontSize:'2vw',
+                                '&:hover': {
+                                color: ' #8584b1',
+                                },
+                            },
+                        }}
+                    />
                 </div>
                 <textarea 
                     id="notes" 
